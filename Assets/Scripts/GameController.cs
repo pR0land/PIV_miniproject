296a77a58358b67ac410b2d49df6a76f48aad25e
Frederik Roland Christiansen <f.roland.c@gmail.com>
2022-11-03 17:23:27 +0100

    Finished the project

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     bool gameEnded = false;
+    [SerializeField] GameObject timeDisplay;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +16,11 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gameEnded)
+        if (gameEnded && timeDisplay.active == false)
         {
             print("Finished");
+            timeDisplay.SetActive(true);
+            timeDisplay.GetComponent<Text>().text = "Your score: " + ((int)Time.timeSinceLevelLoad).ToString();
         }
     }
     public void endGame()
